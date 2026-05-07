bd422d3d5119c57b85222799fe775685abae5372
Hamza <hamzaikhlaq05@gmail.com>
2026-05-07 11:03:01 +0500

    fix: update mongoose and dotenv dependencies

--- application/backend/config/config.js
+++ application/backend/config/config.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose"
 
 const connectDb = async ()=>{
 try {
-    const connect = await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/Ecommerce')
-
+const connect = await mongoose.connect(process.env.MONGO_URI)
     console.log("Database Connected")
  } catch (error) {
     console.log(error.message)
